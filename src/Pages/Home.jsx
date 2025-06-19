1f8e207e55838a21b269dd5c91278cf72f4514fd
Akash Gautam <akash.gautam2024@nst.rishihood.edu.in>
2025-06-20 00:08:44 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,27 +1,36 @@
-import React from 'react';
-import Navbar from '../Section/Navbar'
+import React, { useEffect, useState } from 'react';
+import Navbar from '../Section/Navbar';
 import Hero from '../Section/Hero';
 import Para from '../Section/Para';
-import Why from '../Section/why'
+import Why from '../Section/why';
 import BestSellingSection from '../Section/BestSellingSection';
 import Footer from '../Section/Footer';
-import Every from '../Section/every'
+import Every from '../Section/every';
 import Product from '../Section/product';
 import Faq from '../Section/faq';
+import Loader from '../components/loader';
 
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 4000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) return <Loader />;
+
   return (
     <>
       <Navbar />
-      <Hero/>
-      <Para/>
-      <Why/>
-      <BestSellingSection/>
-      <Every/>
-      <Product/>
-      <Faq/>
-      <Footer/>
-
+      <Hero />
+      <Para />
+      <Why />
+      <BestSellingSection />
+      <Every />
+      <Product />
+      <Faq />
+      <Footer />
     </>
   );
 };
